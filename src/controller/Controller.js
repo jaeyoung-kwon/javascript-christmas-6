7fb36a6d0d32ce668c7193a2fdd2cad8d9947c96
jaeyoung-kwon <61729032+jaeyoung-kwon@users.noreply.github.com>
2024-12-13 13:47:46 +0900

    feat: 입력 받은 방문 날짜 예외 처리

--- src/controller/Controller.js
+++ src/controller/Controller.js
@@ -1,10 +1,24 @@
+import { Console } from '@woowacourse/mission-utils';
+import Parser from '../lib/Parser.js';
+import Validator from '../lib/Validator.js';
 import { InputView, OutputView } from '../view/index.js';
 
 class Controller {
   async start() {
-    const visitDay = await InputView.getVisitDayInput()((input) => input);
+    const visitDay = await this.#getValidatedVisitDay();
     const menuAnsQuantity = await InputView.getMenuAndQuantityInput()((input) => input);
+
+    Console.print(visitDay);
     // OutputView.print();
+  }
+
+  async #getValidatedVisitDay() {
+    return InputView.getVisitDayInput()((input) => {
+      const visitDay = Parser.stringToNumber(input);
+      Validator.validateVisitDayInput(visitDay);
+
+      return visitDay;
+    });
   }
 }
 
